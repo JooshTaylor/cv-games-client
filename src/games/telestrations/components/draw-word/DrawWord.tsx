ad9a0855559ed223b3e594b425eafd92c2a4ff55
Josh <taylorjoshua099@gmail.com>
2021-07-29 11:50:31 +1000

    Get canvas drawing working black and white

--- src/games/telestrations/components/draw-word/DrawWord.tsx
+++ src/games/telestrations/components/draw-word/DrawWord.tsx
@@ -1,15 +1,137 @@
 import React from 'react';
 import { TelestrationsRound } from '../../interfaces/TelestrationsRound';
 
+import './canvas.scss';
+
 interface DrawWordProps {
   round: TelestrationsRound;
 }
 
 export function DrawWord(props: DrawWordProps): JSX.Element {
-  
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+
+  const [ isPainting, setIsPainting ] = React.useState(false);
+  const [ lastX, setLastX ] = React.useState(0);
+  const [ lastY, setLastY ] = React.useState(0);
+  const [ lineThickness, setLineThickness ] = React.useState(1);
+
+  React.useEffect(() => {
+    if (!canvasRef.current)
+      return;
+
+    const canvas = canvasRef.current;
+
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+      
+    canvas.width = canvas.height = 600;
+    ctx.fillRect(0, 0, 600, 600);
+  }, [canvasRef.current]);
+
+  function onMouseDown(e: any) {
+    if (!canvasRef.current)
+      return;
+
+    const ctx = canvasRef.current.getContext('2d') as CanvasRenderingContext2D
+
+    setIsPainting(true);
+    ctx.fillStyle = "#ffffff";
+    setLastX(e.pageX - canvasRef.current.offsetLeft);
+    setLastY(e.pageY - canvasRef.current.offsetTop);
+  }
+
+  function onMouseUp(e: any) {
+    setIsPainting(false);
+  }
+
+  function onMouseMove(e: any) {
+    if (!canvasRef.current)
+      return;
+
+    const ctx = canvasRef.current.getContext('2d') as CanvasRenderingContext2D
+    if (isPainting) {
+      const mouseX = e.pageX - canvasRef.current.offsetLeft;
+      const mouseY = e.pageY - canvasRef.current.offsetTop;
+
+      // find all points between   
+      var x1 = mouseX,
+          x2 = lastX,
+          y1 = mouseY,
+          y2 = lastY;
+
+      var steep = (Math.abs(y2 - y1) > Math.abs(x2 - x1));
+      if (steep){
+          var x = x1;
+          x1 = y1;
+          y1 = x;
+
+          var y = y2;
+          y2 = x2;
+          x2 = y;
+      }
+      if (x1 > x2) {
+          var x = x1;
+          x1 = x2;
+          x2 = x;
+
+          var y = y1;
+          y1 = y2;
+          y2 = y;
+      }
+
+      var dx = x2 - x1,
+          dy = Math.abs(y2 - y1),
+          error = 0,
+          de = dy / dx,
+          yStep = -1,
+          y = y1;
+      
+      if (y1 < y2) {
+          yStep = 1;
+      }
+
+      let newLineThickness = 5 - Math.sqrt((x2 - x1) *(x2-x1) + (y2 - y1) * (y2-y1))/10;
+    
+      if (newLineThickness < 1){
+        newLineThickness = 1;   
+      }
+      
+
+      for (var x = x1; x < x2; x++) {
+          if (steep) {
+              ctx.fillRect(y, x, lineThickness , lineThickness );
+          } else {
+              ctx.fillRect(x, y, lineThickness , lineThickness );
+          }
+          
+          error += de;
+          if (error >= 0.5) {
+              y += yStep;
+              error -= 1.0;
+          }
+      }
+
+      setLastX(mouseX);
+      setLastY(mouseY);
+      setLineThickness(newLineThickness);
+
+  }
+  }
+
   return (
     <>
       <h2>Draw word: {props.round.word}</h2>
+
+      <canvas
+        id="canvas"
+        height="500"
+        width="500"
+        ref={canvasRef}
+        onMouseDown={onMouseDown}
+        onMouseMove={onMouseMove}
+        onMouseUp={onMouseUp}
+      >
+
+      </canvas>
     </>
   );
 }
